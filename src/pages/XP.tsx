77abab531bbee379716d7acb5e9f216405df1d46
yeigen <gabriel.dev.edwards@gmail.com>
2026-07-07 09:42:39 -0500

    imagenes de la xp con texto

--- src/pages/XP.tsx
+++ src/pages/XP.tsx
@@ -1,5 +1,5 @@
 import Abodi from '../assets/experiencia/abodi.png'
-import XP from '../assets/experiencia/logo-uao.png'
+import logo from '../assets/experiencia/logo-uao.png'
 import Indigo from '../assets/experiencia/indigo-logo.jpeg'
 
 function Proyectos() {
@@ -7,9 +7,21 @@ function Proyectos() {
     <div>
       <h1 className="title">EXPERIENCIA</h1>
       <div className='xp-img'>
-      <img src={Abodi} alt="Abodi-Proyecto"/>
-      <img src={XP} alt="Monitor UAO"/>
-      <img src={Indigo} alt="Indigo"/>
+        <figure className='xp-item'>
+          <a href="https://abodiapp.com"><img src={Abodi} alt="Abodi-Proyecto"/></a>
+          <figcaption>Proyecto ABODI</figcaption>
+        </figure>
+
+        <figure className='xp-item'>
+          <a href="https://drive.google.com/file/d/1bdwi5OxVEL2LPY-2pmwOSpAiRUuXpqmV/view?usp=drive_link"><img src={logo} alt="Indigo"/></a>
+          <figcaption>Monitor de Redes e Infraestructura</figcaption>
+      </figure>
+
+      <figure className='xp-item'>
+        <a href="https://indigo.tech/"><img src={Indigo} alt="Indigo"/></a>
+        <figcaption>Junior Developer L1</figcaption>
+      </figure>
+        
       </div>
     </div>
   )
