66af0ce65d830e9b58e16b69e88e17aa8c4ca55b
yeigen <gabriel.dev.edwards@gmail.com>
2026-07-21 19:21:33 -0500

    colocando los links en pestaña nueva y linkeando la foto a abodi

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import yoImage from '../assets/yo_xd.png'
+import yoImage from '../assets/yo-pixel-art.png'
 import Contacto from './Contacto'
 import Proyectos from './XP'
 
@@ -8,9 +8,9 @@ function Home() {
     <div>
         <section id="inicio" className="full-section">
             <h1 className="title">PORTAFOLIO</h1>
-            <p>Ya tenia uno con <a href="https://effortless-raindrop-add9e4.netlify.app/">Astro</a> pero me dio por hacer uno con React</p>
+            <p>Ya tenia uno con <a href="https://effortless-raindrop-add9e4.netlify.app/" target="_blank" rel="noopener noreferrer">Astro</a> pero me dio por hacer uno con React</p>
         <div className='yo-image'>
-            <img src={yoImage} alt="yo" />
+            <a href="https://www.linkedin.com/in/gabriel-eduardo-martinez-martinez-a12068267/" target="_blank" rel="noopener noreferrer"><img src={yoImage} alt="yo" /></a>
         </div>
             <p className='name'>YEIGEN</p>
         </section>
